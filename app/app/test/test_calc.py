b711fa6b068755a4443ce480f31ea383b4dc4ead
Suhas Mohamedali <suhaz04@gmail.com>
2020-08-03 22:29:07 +0930

    Updated build failures

--- app/app/test/test_calc.py
+++ app/app/test/test_calc.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 
 from app.calc import add
 
+
 class CalcTest(TestCase):
     def test_add_numbers(self):
         """Test Adding two numbers"""
-        self.assertEqual(add(3,8), 11)
+        self.assertEqual(add(3, 8), 11)
