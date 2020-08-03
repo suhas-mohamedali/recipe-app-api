4d56020141777c6fc1c3bcb16264b5fd4e0253cb
Suhas Mohamedali <suhaz04@gmail.com>
2020-08-03 22:34:27 +0930

    Updated

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,6 +7,7 @@ class CalcTest(TestCase):
     def test_add_numbers(self):
         """Test Adding two numbers"""
         self.assertEqual(add(3, 8), 11)
+        
     def test_substract(self):
         """Test sunstraction"""
         self.assertEqual(substract(5, 11), 6)
